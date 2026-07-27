6a36a6fb4c3fdb71f1da3c0b8a160ad18232f18b
prodgarbagedestroyer <prodgarbagecollector@gmail.com>
2026-07-27 22:33:46 +0200

    Fix TikTok URL to www.tiktok.com/@prod.garbagedestroyer

--- content/site.ts
+++ content/site.ts
@@ -10,7 +10,7 @@ export const ORG_URLS = {
   youtube: "http://www.youtube.com/@prod.garbagedestroyer",
   github: `https://github.com/${SITE.githubUser}`,
   instagram: "https://instagram.com/prod.garbagedestroyer",
-  tiktok: "https://tiktok.com/@prodgarbagedestroyer",
+  tiktok: "https://www.tiktok.com/@prod.garbagedestroyer",
   facebook: "https://www.facebook.com/profile.php?id=61591399319649",
   email: "mailto:prodgarbagecollector@gmail.com",
 } as const;
